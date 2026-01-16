5a9571a8187f9f285687b91cd9565317c4f36ccf
Pirasak S. <guzzguzzguzz96@gmail.com>
2026-01-16 16:58:36 +0700

    update logo icon

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -362,7 +362,7 @@ export default function AdminLayout({ children }) {
           {/* LOGO + TITLE */}
           <div className="relative flex items-center px-2">
             <img
-              src="/logo-9exp-sec.png"
+              src="/logo-9exp-sec.ico"
               alt="logo"
               width={32}
               height={32}
